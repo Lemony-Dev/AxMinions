8f1ed01a29732780c2304575e53c0fa4069836ed
Lemony <lemonymc82@gmail.com>
2026-09-23 21:58:40 +0300

    Update WarningContainerFull.kt

--- api/src/main/kotlin/com/artillexstudios/axminions/api/warnings/impl/WarningContainerFull.kt
+++ api/src/main/kotlin/com/artillexstudios/axminions/api/warnings/impl/WarningContainerFull.kt
@@ -2,10 +2,11 @@ package com.artillexstudios.axminions.api.warnings.impl
 
 import com.artillexstudios.axminions.api.config.Messages
 import com.artillexstudios.axminions.api.warnings.Warning
+import com.artillexstudios.axminions.api.minions.Minion
 
 class WarningContainerFull : Warning("container_full") {
 
-    override fun getContent(): String {
+    override fun getContent(minion: Minion): String {
         return Messages.CONTAINER_FULL_WARNING()
     }
 }
