812e4f98259d685ca361a2adac261db4c8c9ebfb
Lemony <lemonymc82@gmail.com>
2026-09-23 21:59:55 +0300

    Update WarningNoTool.kt

--- api/src/main/kotlin/com/artillexstudios/axminions/api/warnings/impl/WarningNoTool.kt
+++ api/src/main/kotlin/com/artillexstudios/axminions/api/warnings/impl/WarningNoTool.kt
@@ -2,10 +2,11 @@ package com.artillexstudios.axminions.api.warnings.impl
 
 import com.artillexstudios.axminions.api.config.Messages
 import com.artillexstudios.axminions.api.warnings.Warning
+import com.artillexstudios.axminions.api.minions.Minion
 
 class WarningNoTool : Warning("no_tool") {
 
-    override fun getContent(): String {
+    override fun getContent(minion Minion): String {
         return Messages.NO_TOOL_WARNING()
     }
 }
