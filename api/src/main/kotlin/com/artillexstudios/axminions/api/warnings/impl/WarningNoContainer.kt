b48ccdb02fb50ef52eb45e0d25f22b20bab55b00
Lemony <lemonymc82@gmail.com>
2026-09-23 21:58:05 +0300

    Update WarningNoContainer.kt

--- api/src/main/kotlin/com/artillexstudios/axminions/api/warnings/impl/WarningNoContainer.kt
+++ api/src/main/kotlin/com/artillexstudios/axminions/api/warnings/impl/WarningNoContainer.kt
@@ -2,10 +2,11 @@ package com.artillexstudios.axminions.api.warnings.impl
 
 import com.artillexstudios.axminions.api.config.Messages
 import com.artillexstudios.axminions.api.warnings.Warning
+import com.artillexstudios.axminions.api.minions.Minion
 
 class WarningNoContainer : Warning("no_container") {
 
-    override fun getContent(): String {
+    override fun getContent(minion: Minion): String {
         return Messages.NO_CONTAINER_WARNING()
     }
 }
