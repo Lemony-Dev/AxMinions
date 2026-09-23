a527c56d4521c21ab779493850bb84ca88656823
Lemony <lemonymc82@gmail.com>
2026-09-23 21:59:07 +0300

    Update WarningNoWaterNearby.kt

--- api/src/main/kotlin/com/artillexstudios/axminions/api/warnings/impl/WarningNoWaterNearby.kt
+++ api/src/main/kotlin/com/artillexstudios/axminions/api/warnings/impl/WarningNoWaterNearby.kt
@@ -2,10 +2,10 @@ package com.artillexstudios.axminions.api.warnings.impl
 
 import com.artillexstudios.axminions.api.config.Messages
 import com.artillexstudios.axminions.api.warnings.Warning
-
+import com.artillexstudios.axminions.api.minions.Minion
 class WarningNoWaterNearby : Warning("no_water_nearby") {
 
-    override fun getContent(): String {
+    override fun getContent(minion: Minion): String {
         return Messages.NO_WATER_NEARBY_WARNING()
     }
 }
